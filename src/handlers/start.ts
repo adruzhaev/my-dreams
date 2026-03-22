4153b7a32476c5d6eeecdd958b7862eb7cd04817
Andrew <andrewdruzhaev@gmail.com>
2026-03-22 20:38:22 +0500

    remove debug log

--- src/handlers/start.ts
+++ src/handlers/start.ts
@@ -1,6 +1,5 @@
 import { MyContext } from "../types/context";
 
 export async function startHandler(ctx: MyContext) {
-  console.log("ctx.from.language_code: ", ctx?.from?.language_code);
   await ctx.reply(ctx.t("welcome"), { parse_mode: "Markdown" });
 }
